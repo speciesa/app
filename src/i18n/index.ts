44d89205c725bd0f6b9ccb221648a9e755c38626
Aleh Samoilau <aleh_samoilau@epam.com>
2026-05-08 21:00:19 +0200

    it is compiled but not working on runtime

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,9 +4,9 @@ import ru from './locales/ru';
 import en from './locales/en';
 
 const i18n = new I18n({ ru, en });
-i18n.locale = Localization.getLocales()[0]?.languageCode ?? 'ru';
+i18n.locale = Localization.getLocales()[0]?.languageCode ?? 'en';
 i18n.enableFallback = true;
-i18n.defaultLocale = 'ru';
+i18n.defaultLocale = 'en';
 
 export default i18n;
 export const t = (key: string, opts?: object) => i18n.t(key, opts);
